45a9ad7d1bcf17cf82bcfa2d1ef5a99f991db248
Diego Giraldo <dgiraldogaleano@gmail.com>
2023-05-29 11:54:16 -0500

    tipos y estados de las variables en js

---
+++ variables.js
@@ -0,0 +1,14 @@
+var nombre = "oscar"
+var edad; // declarar
+edad = 52; // inicializar
+
+var elementos = ["computador", "celular"]
+elementos [1]
+
+var persona:{
+    nombre: "diego",
+    edad: 50
+
+}
+
+
