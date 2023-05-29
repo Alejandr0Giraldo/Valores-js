de16a920e2e3601a357832c609be28857d0b11ce
Diego Giraldo <dgiraldogaleano@gmail.com>
2023-05-29 11:32:46 -0500

    valores

---
+++ valores.js
@@ -0,0 +1,14 @@
+40 // number
+
+"Diego" // string
+
+True,False // bool
+
+null
+undefined // valores vacios
+
+[1,2,3,4] // array
+
+{nombre: "Diego"} // objeto
+
+typeof // palabra reservada para saber tipo de dato
