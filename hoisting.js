775cc5428520901a024e909388c09f753d771240
Diego Giraldo <dgiraldogaleano@gmail.com>
2023-05-31 10:53:59 -0500

    operaciones con funciones

---
+++ hoisting.js
@@ -0,0 +1,12 @@
+console.log(miNombre);
+
+var miNombre = "Diego";
+
+
+var miNombre = undefined
+
+console.log(miNombre + "soy ese hoisting")
+
+miNombre = "Diego"
+
+//el hoisting es cuando las variables y las funciones se declaran antes de procesar cualquier codigo
